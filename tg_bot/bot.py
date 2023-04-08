46c8b438881ba8a1776da637084ff7c3d2b5e0d7
Danil-11495722 <ejyou.user@gmail.com>
2023-04-08 09:08:40 +0300

    Add to constants both paths for Windows and Linux

--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -2,10 +2,11 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from config import token
-import constants
+from constants import PROJECT_PATH
+
 
 badwords = []
-with open(f"{constants.PROJECT_PATH}/tg_bot/cenz.json", "r", encoding="utf-8") as cenz:
+with open (f"{PROJECT_PATH}/tg_bot/cenz.json", "r", encoding="utf-8") as cenz:
    badwords = eval(cenz.read())
 
 bot = Bot(token=token)
@@ -20,7 +21,7 @@ async def cmd_start(message: types.Message):
 @dp.message_handler(commands=["AddWord"])
 async def Add_Сenz_Filter(message: types.Message):
     badwords.append(message.text.lower()[9:])
-    with open("cenz.json", "w", encoding="utf-8") as jn:
+    with open(f"{PROJECT_PATH}/tg_bot/cenz.json", "w", encoding="utf-8") as jn:
         jn.write(str(badwords))
     await message.reply("Слово " + message.text[9:] + " добавлено в список")
 
