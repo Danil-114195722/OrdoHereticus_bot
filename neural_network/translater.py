3c2d2be191577e3476ac3b6b7c6cc1db42d8ded0
Danil-11495722 <ejyou.user@gmail.com>
2023-04-08 10:32:51 +0300

    make enron_former for copy en file

--- neural_network/translater.py
+++ neural_network/translater.py
@@ -1,16 +1,26 @@
 import pandas as pd
 from googletrans import Translator
-from tg_bot import constants
+from OrdoHereticus_bot import constants
+
+import asyncio
 
 
-df = pd.read_csv(f'{constants.PROJECT_PATH}/dataset/spam.csv', encoding='cp1251')
-translator = Translator()
+def translate_csv(from_file: str, to_file: str) -> None:
+    df = pd.read_csv(from_file, encoding='cp1251')
+    translator = Translator()
 
-russian_texts = []
-for num, texts in enumerate(df['message'], 0):
-    russian_text = translator.translate(texts, src='en', dest='ru', encodings='cp1251').text
-    russian_texts.append(russian_text)
-    print(russian_text)
-    print(num)
-df['russian_text'] = russian_texts
-df.to_csv(f'{constants.PROJECT_PATH}/dataset/spam_russian.csv', index=False)
+    russian_texts = []
+    for num, texts in enumerate(df['message'], 0):
+        russian_text = translator.translate(texts, src='en', dest='ru', encodings='cp1251').text
+        russian_texts.append(russian_text)
+        print(russian_text)
+        print(num)
+    df['russian_text'] = russian_texts
+    df.to_csv(to_file, index=False)
+
+
+if __name__ == '__main__':
+    translate_csv(
+        f'{constants.PROJECT_PATH}/dataset/spam.csv',
+        f'{constants.PROJECT_PATH}/dataset/spam_russian.csv'
+    )
