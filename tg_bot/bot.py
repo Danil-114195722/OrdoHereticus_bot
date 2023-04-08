ba60ba6e8d74df4348cd6709cc578105a81ab320
Danil-11495722 <ejyou.user@gmail.com>
2023-04-08 08:21:20 +0300

    Fix bot bugs

--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -1,20 +1,21 @@
-import asyncio, json
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from config import token
+from constants import PROJECT_PATH
 
 badwords = []
-with open ("cenz.json", "r", encoding="utf-8") as cenz:
+with open (f"{PROJECT_PATH}/tg_bot/cenz.json", "r", encoding="utf-8") as cenz:
    badwords = eval(cenz.read())
 
 bot = Bot(token=token)
 dp = Dispatcher(bot)
 
+
 @dp.message_handler(commands = ["start", "help"])
 async def cmd_start(message: types.Message):
     await message.answer("Бот работает")
-    pass
+
 
 @dp.message_handler(commands=["AddWord"])
 async def Add_Сenz_Filter(message: types.Message):
@@ -23,11 +24,13 @@ async def Add_Сenz_Filter(message: types.Message):
         jn.write(str(badwords))
     await message.reply("Слово " + message.text[9:] + " добавлено в список")
 
+
 @dp.message_handler()
 async def Сenz_Filter(message: types.Message):
     for i in message.text.lower().split(" "):
         if i in badwords:
             await message.reply("ЗА ИМПЕРАТОРА!!!")
             await message.delete()
+
 
 executor.start_polling(dp, skip_updates=True)
