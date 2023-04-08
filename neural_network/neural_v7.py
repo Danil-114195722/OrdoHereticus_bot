cc205fdc6434c0bacf7b68a7c304951764b65c11
Alexey <a-nes.rnd@mai.ru>
2023-04-08 16:05:27 +0300

    Update neural.py to 7 version Update spam_classifier.h5 Add neural_test.py Update spam_russian.csv

--- neural_network/neural_v7.py
+++ neural_network/neural_v7.py
@@ -1,12 +1,14 @@
-from langdetect.lang_detect_exception import LangDetectException
-from keras.preprocessing.text import Tokenizer
-from keras.utils import pad_sequences
 import tensorflow as tf
-import pandas as pd
+from tensorflow.keras.layers import Dense, LSTM, Embedding, Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 import langdetect
-import constants
+from langdetect.lang_detect_exception import LangDetectException
+import pandas as pd
+import numpy as np
 import re
-
+import constants
 
 # Create labels language
 labels_en = ['не спам', 'спам']
@@ -33,6 +35,40 @@ seq = tokenizer.texts_to_sequences(messages)
 # Padding to approach all messages to the same length
 max_len = 100  # Defining the maximum length of sentence
 X = pad_sequences(seq, maxlen=max_len)
+
+# Splitting the dataset into train and test
+test_ratio = 0.25
+indices = np.arange(X.shape[0])
+np.random.shuffle(indices)
+X = X[indices]
+y = np.array(label)[indices]
+
+train_size = int(X.shape[0] * 0.75)
+
+X_train, X_test = X[:train_size], X[train_size:]
+y_train, y_test = y[:train_size], y[train_size:]
+
+# Define the model
+embedding_dim = 32  # Dimension of the token embedding
+model = Sequential()
+model.add(Embedding(max_features, embedding_dim, input_length=X.shape[1]))
+model.add(LSTM(64, dropout=0.1, recurrent_dropout=0.1, return_sequences=True))
+model.add(LSTM(32, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, epochs=5, batch_size=64, validation_split=0.2)
+
+# Evaluate the model
+loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+print(f'Test loss: {loss}')
+print(f'Test accuracy: {accuracy}')
+
+# Save the model for later use
+model.save('spam_classifier.h5')
 
 # To classify new messages in the future, load the trained model
 loaded_model = tf.keras.models.load_model(f'{constants.PROJECT_PATH}/neural_network/spam_classifier.h5')
